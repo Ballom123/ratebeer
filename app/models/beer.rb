1242e75fc1e791a5384f28b2155ce05622c81034
Jesper <jesper.kuusela@gmail.com>
2023-09-12 14:35:50 +0300

    wk 2 ex 5 enumerables, need to add rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ApplicationRecord
+    include Enumerable
+
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        array = Rating.where "beer_id= #{self.id}"
+        return (array.average(:score)).to_f
+    end
+
+    def total_ratings
+        array = Rating.where "beer_id= #{self.id}"
+        sum = array.size
+        return sum
+    end
+
 end
